943aa3c25ceb4e47dddea2b585266e791031528b
Sadiq. <ms0205y@gre.ac.uk>
2024-02-16 18:17:49 +0000

    Some errors are fixed

--- Coordinator.java
+++ Coordinator.java
@@ -3,31 +3,22 @@ import java.util.*;
 public class Coordinator extends Member {
     private List<Member> activeMembers;
 
-    public Coordinator(String id, Socket socket) {
-        super(id, socket);
+    public Coordinator(String id, String ipAddress, int port) {
+        super(id, ipAddress, port);
         this.activeMembers = new ArrayList<>();
     }
 
     public void maintainState() {
-        // This method should be called periodically (e.g., every 20 seconds)
-        // to check the state of active group members. You could implement this
-        // using a Timer or ScheduledExecutorService.
-
-        // For now, let's just print out the IDs of the active members
         System.out.println("Active members:");
         for (Member member : activeMembers) {
-            System.out.println(member.getId());
+            System.out.println(member.toString());
         }
     }
 
     public String handleDetailsRequest() {
-        // This method should return the details of all active members,
-        // including their IDs, IP addresses, and ports. For now, let's just
-        // return a string with their IDs.
-
         StringBuilder details = new StringBuilder();
         for (Member member : activeMembers) {
-            details.append(member.getId()).append("\n");
+            details.append(member.toString()).append("\n");
         }
         return details.toString();
     }
